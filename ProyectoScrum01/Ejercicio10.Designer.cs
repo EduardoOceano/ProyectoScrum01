99b9ab3ee33c0c5c90999a46a7ff06788cf685c8
Jose Luis Gardeta <prutote@gmail.com>
2021-04-14 11:22:35 +0200

    Signed-off-by: Jose Luis Gardeta <prutote@gmail.com>

---
+++ ProyectoScrum01/Ejercicio10.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ProyectoScrum01
+{
+    partial class Ejercicio10
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtVector = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtVector
+            // 
+            this.txtVector.Location = new System.Drawing.Point(58, 33);
+            this.txtVector.Name = "txtVector";
+            this.txtVector.Size = new System.Drawing.Size(161, 22);
+            this.txtVector.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(250, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Mostrar nº mas grande";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Enabled = false;
+            this.txtNumero.Location = new System.Drawing.Point(58, 90);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(161, 22);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // Ejercicio10
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtVector);
+            this.Name = "Ejercicio10";
+            this.Text = "Ejercicio10";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtVector;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNumero;
+    }
+}
