a17c0303b34e117120eca40bf09988b8c04f7e32
Javier <jvivas1996@gmail.com>
2021-04-14 13:19:47 +0200

    FINAL

--- ProyectoScrum01/Form1.Designer.cs
+++ ProyectoScrum01/Form1.Designer.cs
@@ -29,73 +29,133 @@ namespace ProyectoScrum01
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.btnEjercicio7 = new System.Windows.Forms.Button();
-            this.btnEjercicio8 = new System.Windows.Forms.Button();
-            this.btnEjercicio6 = new System.Windows.Forms.Button();
+            this.button08 = new System.Windows.Forms.Button();
+            this.button07 = new System.Windows.Forms.Button();
+            this.button06 = new System.Windows.Forms.Button();
+            this.button05 = new System.Windows.Forms.Button();
+            this.button04 = new System.Windows.Forms.Button();
+            this.button03 = new System.Windows.Forms.Button();
+            this.button02 = new System.Windows.Forms.Button();
+            this.button01 = new System.Windows.Forms.Button();
+            this.button09 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // button08
             // 
-            this.button1.Location = new System.Drawing.Point(380, 11);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button08.Location = new System.Drawing.Point(27, 259);
+            this.button08.Name = "button08";
+            this.button08.Size = new System.Drawing.Size(94, 23);
+            this.button08.TabIndex = 3;
+            this.button08.Text = "Ejercicio 08";
+            this.button08.UseVisualStyleBackColor = true;
+            this.button08.Click += new System.EventHandler(this.btnEjercicio8_Click);
             // 
-            // button2
+            // button07
             // 
-            this.button2.Location = new System.Drawing.Point(462, 13);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 28);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button07.Location = new System.Drawing.Point(27, 229);
+            this.button07.Name = "button07";
+            this.button07.Size = new System.Drawing.Size(94, 23);
+            this.button07.TabIndex = 2;
+            this.button07.Text = "Ejercicio 07";
+            this.button07.UseVisualStyleBackColor = true;
+            this.button07.Click += new System.EventHandler(this.btnEjercicio7_Click);
             // 
-            // btnEjercicio7
+            // button06
             // 
-            this.btnEjercicio7.Location = new System.Drawing.Point(361, 133);
-            this.btnEjercicio7.Name = "btnEjercicio7";
-            this.btnEjercicio7.Size = new System.Drawing.Size(94, 23);
-            this.btnEjercicio7.TabIndex = 2;
-            this.btnEjercicio7.Text = "Ejercicio 07";
-            this.btnEjercicio7.UseVisualStyleBackColor = true;
-            this.btnEjercicio7.Click += new System.EventHandler(this.btnEjercicio7_Click);
+            this.button06.Location = new System.Drawing.Point(27, 200);
+            this.button06.Name = "button06";
+            this.button06.Size = new System.Drawing.Size(94, 23);
+            this.button06.TabIndex = 4;
+            this.button06.Text = "Ejercicio 06";
+            this.button06.UseVisualStyleBackColor = true;
+            this.button06.Click += new System.EventHandler(this.btnEjercicio6_Click);
             // 
-            // btnEjercicio8
+            // button05
             // 
-            this.btnEjercicio8.Location = new System.Drawing.Point(361, 163);
-            this.btnEjercicio8.Name = "btnEjercicio8";
-            this.btnEjercicio8.Size = new System.Drawing.Size(94, 23);
-            this.btnEjercicio8.TabIndex = 3;
-            this.btnEjercicio8.Text = "Ejercicio 08";
-            this.btnEjercicio8.UseVisualStyleBackColor = true;
-            this.btnEjercicio8.Click += new System.EventHandler(this.btnEjercicio8_Click);
+            this.button05.Location = new System.Drawing.Point(27, 171);
+            this.button05.Name = "button05";
+            this.button05.Size = new System.Drawing.Size(94, 23);
+            this.button05.TabIndex = 5;
+            this.button05.Text = "Ejercicio 05";
+            this.button05.UseVisualStyleBackColor = true;
+            this.button05.Click += new System.EventHandler(this.button05_Click);
             // 
-            // btnEjercicio6
+            // button04
             // 
-            this.btnEjercicio6.Location = new System.Drawing.Point(361, 104);
-            this.btnEjercicio6.Name = "btnEjercicio6";
-            this.btnEjercicio6.Size = new System.Drawing.Size(94, 23);
-            this.btnEjercicio6.TabIndex = 4;
-            this.btnEjercicio6.Text = "Ejercicio 06";
-            this.btnEjercicio6.UseVisualStyleBackColor = true;
-            this.btnEjercicio6.Click += new System.EventHandler(this.btnEjercicio6_Click);
+            this.button04.Location = new System.Drawing.Point(27, 142);
+            this.button04.Name = "button04";
+            this.button04.Size = new System.Drawing.Size(94, 23);
+            this.button04.TabIndex = 6;
+            this.button04.Text = "Ejercicio 04";
+            this.button04.UseVisualStyleBackColor = true;
+            this.button04.Click += new System.EventHandler(this.button04_Click);
+            // 
+            // button03
+            // 
+            this.button03.Location = new System.Drawing.Point(27, 113);
+            this.button03.Name = "button03";
+            this.button03.Size = new System.Drawing.Size(94, 23);
+            this.button03.TabIndex = 7;
+            this.button03.Text = "Ejercicio 03";
+            this.button03.UseVisualStyleBackColor = true;
+            this.button03.Click += new System.EventHandler(this.button03_Click);
+            // 
+            // button02
+            // 
+            this.button02.Location = new System.Drawing.Point(27, 84);
+            this.button02.Name = "button02";
+            this.button02.Size = new System.Drawing.Size(94, 23);
+            this.button02.TabIndex = 8;
+            this.button02.Text = "Ejercicio 02";
+            this.button02.UseVisualStyleBackColor = true;
+            this.button02.Click += new System.EventHandler(this.button02_Click);
+            // 
+            // button01
+            // 
+            this.button01.Location = new System.Drawing.Point(27, 55);
+            this.button01.Name = "button01";
+            this.button01.Size = new System.Drawing.Size(94, 23);
+            this.button01.TabIndex = 9;
+            this.button01.Text = "Ejercicio 01";
+            this.button01.UseVisualStyleBackColor = true;
+            this.button01.Click += new System.EventHandler(this.button01_Click);
+            // 
+            // button09
+            // 
+            this.button09.Location = new System.Drawing.Point(27, 288);
+            this.button09.Name = "button09";
+            this.button09.Size = new System.Drawing.Size(94, 23);
+            this.button09.TabIndex = 10;
+            this.button09.Text = "Ejercicio 09";
+            this.button09.UseVisualStyleBackColor = true;
+            this.button09.Click += new System.EventHandler(this.button09_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(27, 317);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(94, 23);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Ejercicio 10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button7_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnEjercicio6);
-            this.Controls.Add(this.btnEjercicio8);
-            this.Controls.Add(this.btnEjercicio7);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button09);
+            this.Controls.Add(this.button01);
+            this.Controls.Add(this.button02);
+            this.Controls.Add(this.button03);
+            this.Controls.Add(this.button04);
+            this.Controls.Add(this.button05);
+            this.Controls.Add(this.button06);
+            this.Controls.Add(this.button08);
+            this.Controls.Add(this.button07);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -104,12 +164,16 @@ namespace ProyectoScrum01
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button btnEjercicio7;
-        private System.Windows.Forms.Button btnEjercicio8;
-        private System.Windows.Forms.Button btnEjercicio6;
+        private System.Windows.Forms.Button button08;
+        private System.Windows.Forms.Button button07;
+        private System.Windows.Forms.Button button06;
+        private System.Windows.Forms.Button button05;
+        private System.Windows.Forms.Button button04;
+        private System.Windows.Forms.Button button03;
+        private System.Windows.Forms.Button button02;
+        private System.Windows.Forms.Button button01;
+        private System.Windows.Forms.Button button09;
+        private System.Windows.Forms.Button button10;
     }
 }
 
--- ProyectoScrum01/Form1.cs
+++ ProyectoScrum01/Form1.cs
@@ -35,5 +35,47 @@ namespace ProyectoScrum01
             Ejercicio6 ejercicio6 = new Ejercicio6();
             ejercicio6.Show();
         }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Ejercicio10 ejercicio10 = new Ejercicio10();
+            ejercicio10.Show();
+        }
+
+        private void button01_Click(object sender, EventArgs e)
+        {
+            Ejercicio1 ejercicio1 = new Ejercicio1();
+            ejercicio1.Show();
+        }
+
+        private void button02_Click(object sender, EventArgs e)
+        {
+            Ejercicio2 ejercicio2 = new Ejercicio2();
+            ejercicio2.Show();
+        }
+
+        private void button03_Click(object sender, EventArgs e)
+        {
+            Ejercicio3 ejercicio3 = new Ejercicio3();
+            ejercicio3.Show();
+        }
+
+        private void button04_Click(object sender, EventArgs e)
+        {
+            Ejercicio4 ejercicio4 = new Ejercicio4();
+            ejercicio4.Show();
+        }
+
+        private void button05_Click(object sender, EventArgs e)
+        {
+            Ejercicio5 ejercicio5 = new Ejercicio5();
+            ejercicio5.Show();
+        }
+
+        private void button09_Click(object sender, EventArgs e)
+        {
+            Ejercicio9 ejercicio9 = new Ejercicio9();
+            ejercicio9.Show();
+        }
     }
 }
