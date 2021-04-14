d5443810fd33b62f0df80a3a67d837030ba99bf3
Eduardo <eduardochiscuet@fp.oceanoatlantico.org>
2021-04-14 12:43:37 +0200

    commit test

--- ProyectoScrum01/Program.cs
+++ ProyectoScrum01/Program.cs
@@ -16,7 +16,7 @@ namespace ProyectoScrum01
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Ejercicio4());
+            Application.Run(new Form1());
         }
     }
 }
